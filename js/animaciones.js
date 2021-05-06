47dc586832ee63ed61bc27ee3994482827c75542
Manuel <ferman5903@gmail.com>
2021-05-05 23:58:23 -0500

    Mejoras CSS - Tecnologías (node, firebase) - Grid Proyectos

--- js/animaciones.js
+++ js/animaciones.js
@@ -4,8 +4,11 @@ window.addEventListener('load', ()=>{
 	animar(".contacto.anim-aparecer",1.5,window.innerHeight);
 })
 window.addEventListener('scroll', ()=>{
+	animar(".anim-arriba-aparecer", 1, window.innerHeight-100);
+	animar(".anim-izquierda-aparecer");
 	animar(".anim-aparecer");
-	animar(".anim-derecha");
+	animar(".anim-aparecer", 1, window.innerHeight - 100)
+	animar(".anim-derecha", 1, window.innerHeight/1.5);
 	document.querySelectorAll('.tecnologia.anim-derecha-aparecer').forEach((e,i)=>{
 		animar(e,(i+1)*.5,window.innerHeight/1.5);
 	})
--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,45 @@
-const menu = document.querySelector('.menu');
+const navbar = document.querySelector('.navbar');
 
 let ultimaPos = 0;
 
 window.addEventListener('scroll', () => {
 	//Cambio de estilos
 	if(window.scrollY > 0){
-		menu.classList.add("abajo");
+		navbar.classList.add("abajo");
 	} else {
-		menu.classList.remove("abajo");
+		navbar.classList.remove("abajo");
 	}
 	//Esconder o sacar menú
 	if(window.scrollY > 300){
 		if(window.scrollY > ultimaPos){
-			menu.style.transform = "translateY(-100%)";
+			navbar.style.transform = "translateY(-100%)";
 		} else {
-			menu.style.transform = "translateY(0)";
+			navbar.style.transform = "translateY(0)";
 		}
 	}
 	ultimaPos = window.scrollY;
+})
+
+// DISPOSITIVOS MOVILES
+const hamburguesa = document.querySelector('.hamburguesa');
+const menuMobile = document.querySelector('.menu-mobile');
+const btnCerrar = document.querySelector('.menu-mobile .cerrar');
+const menuMobileItems = document.querySelectorAll('.menu-mobile .menu__item');
+
+hamburguesa.addEventListener('click', () => {
+	menuMobile.classList.add('activo');
+})
+
+btnCerrar.addEventListener('click', () => {
+	menuMobile.classList.remove('activo');
+})
+
+menuMobileItems.forEach(e => {
+	e.addEventListener('click', () => {
+		menuMobile.classList.remove('activo');
+	})
+})
+
+window.addEventListener('resize', () => {
+	menuMobile.classList.remove('activo');
 })
