36e528ba73786aae8c0ada6577872780f57ba3dd
Manuel De Alba <ferman5903@gmail.com>
2024-06-21 02:35:34 -0600

    Add projects details pages

---
+++ src/proyectos.js
@@ -0,0 +1,66 @@
+export default [
+    {
+        id: 1,
+        src: "/harty.jpg",
+        title: "Harty",
+        description: ["Sitio web para la publicación y renta de terrazas. Cuenta con un sistema de autenticación, gestión de roles y herramientas para una visualización amigable."],
+        technologies: ["html", "css", "js", "react", "firebase"],
+        github: "https://github.com/ManuelDeAlba/harty",
+        web: "https://is-harty.netlify.app"
+    },
+    {
+        id: 2,
+        src: "/skillup.jpg",
+        title: "SkillUp",
+        description: ["Plataforma web que permite que los estudiantes encuentren oportunidades de trabajo, facilitando su inserción en el mercado laboral."],
+        technologies: ["html", "css", "js", "react", "firebase"],
+        github: "https://github.com/ManuelDeAlba/skillup",
+        web: "https://is-skillup.netlify.app"
+    },
+    {
+        id: 3,
+        src: "/photographyorozco.jpg",
+        title: "Photographyorozco",
+        description: ["Portafolio web Vanesa Orozco Photography - Fotografía profesional para Bodas, XV Años, Familia, Infantil"],
+        technologies: ["html", "css", "js", "next"],
+        web: "https://photographyorozco.com/",
+        testimonials: [
+            {
+                name: "Vanesa Orozco",
+                testimonial: "Manuel es un excelente profesional, siempre dispuesto a ayudar y aportar ideas para mejorar el proyecto. Su trabajo es de calidad y siempre cumple con los plazos de entrega. ¡Lo recomiendo ampliamente!"
+            }
+        ]
+    },
+    {
+        id: 4,
+        src: "/homestockessentials.jpg",
+        title: "Homestockessentials",
+        description: ["Sitio web para la búsqueda y filtrado en el catálogo de productos."],
+        technologies: ["html", "css", "js", "next", "firebase"],
+        web: "https://homestockessentials.vercel.app/"
+    },
+    {
+        id: 5,
+        src: "/stockessentials.jpg",
+        title: "Stockessentials",
+        description: ["Plataforma para gestión del inventario de artículos varios para el hogar. Con sistema de autenticación y herramientas para registrar compras, ventas, productos apartados, ingresos, egresos y registro de movimientos."],
+        technologies: ["html", "css", "js", "react", "firebase"]
+    },
+    {
+        id: 6,
+        src: "/wikirickymorty.jpg",
+        title: "Wiki Rick and Morty",
+        description: ["Wiki de Rick & Morty, con información de los personajes, episodios y ubicaciones de la serie utilizando React.js y la API de Rick & Morty."],
+        technologies: ["html", "css", "js", "react"],
+        github: "https://github.com/ManuelDeAlba/rickandmortyapi",
+        web: "https://wikirickymorty.netlify.app/"
+    },
+    {
+        id: 7,
+        src: "/vivero.jpg",
+        title: "FullStack App Vivero",
+        description: ["Frontend y backend para la administración de los proveedores, productos, compras y ventas de un vivero."],
+        technologies: ["html", "css", "js", "react", "node", "sequelize", "postgresql"],
+        github: "https://github.com/ManuelDeAlba/FullStack-App-Vivero"
+    }
+]
