7f9c47a290962491d6d88c62d4739d2d49d610b5
Manuel De Alba <ferman5903@gmail.com>
2025-02-27 01:24:26 -0600

    Optimize project images

--- src/proyectos.js
+++ src/proyectos.js
@@ -1,7 +1,9 @@
+const PROJECTS_IMAGES_DIR = "/proyectos";
+
 export default [
     {
         id: 8,
-        src: "/bangstaymd.png",
+        src: `${PROJECTS_IMAGES_DIR}/bangstaymd.webp`,
         title: "Bangstaymd",
         description: ["Sitio web para la publicación de postres. Incluye una forma para que el cliente se comunique con el vendedor por medio de whatsapp y haga un pedido de forma sencilla, permitiendo seleccionar los productos y rellenando los datos necesarios para automatizar el proceso de comunicación."],
         technologies: ["html", "css", "js", "astro"],
@@ -10,7 +12,7 @@ export default [
     },
     {
         id: 1,
-        src: "/harty.jpg",
+        src: `${PROJECTS_IMAGES_DIR}/harty.webp`,
         title: "Harty",
         description: ["Sitio web para la publicación y renta de terrazas. Cuenta con un sistema de autenticación, gestión de roles y herramientas para una visualización amigable."],
         technologies: ["html", "css", "js", "react", "firebase"],
@@ -19,7 +21,7 @@ export default [
     },
     {
         id: 2,
-        src: "/skillup.jpg",
+        src: `${PROJECTS_IMAGES_DIR}/skillup.webp`,
         title: "SkillUp",
         description: ["Plataforma web que permite que los estudiantes encuentren oportunidades de trabajo, facilitando su inserción en el mercado laboral."],
         technologies: ["html", "css", "js", "react", "firebase"],
@@ -28,7 +30,7 @@ export default [
     },
     {
         id: 3,
-        src: "/photographyorozco.jpg",
+        src: `${PROJECTS_IMAGES_DIR}/photographyorozco.webp`,
         title: "Photographyorozco",
         description: ["Portafolio web Vanesa Orozco Photography - Fotografía profesional para Bodas, XV Años, Familia, Infantil"],
         technologies: ["html", "css", "js", "next"],
@@ -42,7 +44,7 @@ export default [
     },
     {
         id: 4,
-        src: "/homestockessentials.jpg",
+        src: `${PROJECTS_IMAGES_DIR}/homestockessentials.webp`,
         title: "Homestockessentials",
         description: ["Sitio web para la búsqueda y filtrado en el catálogo de productos."],
         technologies: ["html", "css", "js", "next", "firebase"],
@@ -50,14 +52,14 @@ export default [
     },
     {
         id: 5,
-        src: "/stockessentials.jpg",
+        src: `${PROJECTS_IMAGES_DIR}/stockessentials.webp`,
         title: "Stockessentials",
         description: ["Plataforma para gestión del inventario de artículos varios para el hogar. Con sistema de autenticación y herramientas para registrar compras, ventas, productos apartados, ingresos, egresos y registro de movimientos."],
         technologies: ["html", "css", "js", "react", "firebase"]
     },
     // {
     //     id: 6,
-    //     src: "/wikirickymorty.jpg",
+    //     src: `${PROJECTS_IMAGES_DIR}/wikirickymorty.webp`,
     //     title: "Wiki Rick and Morty",
     //     description: ["Wiki de Rick & Morty, con información de los personajes, episodios y ubicaciones de la serie utilizando React.js y la API de Rick & Morty."],
     //     technologies: ["html", "css", "js", "react"],
@@ -66,7 +68,7 @@ export default [
     // },
     {
         id: 7,
-        src: "/vivero.jpg",
+        src: `${PROJECTS_IMAGES_DIR}/vivero.webp`,
         title: "FullStack App Vivero",
         description: ["Frontend y backend para la administración de los proveedores, productos, compras y ventas de un vivero."],
         technologies: ["html", "css", "js", "react", "node", "sequelize", "postgresql"],
